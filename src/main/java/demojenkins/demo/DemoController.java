2d2092dc40bdc373b0cfd0dad4e42187d0599f9e
thanhdinh01 <thanhdinhduy0301@gmail.com>
2023-12-13 14:15:37 +0700

    upcode

--- src/main/java/demojenkins/demo/DemoController.java
+++ src/main/java/demojenkins/demo/DemoController.java
@@ -14,4 +14,5 @@ public class DemoController {
     public String contact(){
         return "Hello you";
     }
+
 }
---
+++ src/test/java/demojenkins/demo/DemoControllerTest.java
@@ -0,0 +1,20 @@
+package demojenkins.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemoControllerTest {
+    private final DemoController demoController = new DemoController();
+    @Test
+    void about() {
+//        DemoController demoController= new DemoController();
+        assertEquals("Hello world1",demoController.about());
+    }
+
+    @Test
+    void contact() {
+        assertEquals("Hello you",demoController.contact());
+    }
+}
