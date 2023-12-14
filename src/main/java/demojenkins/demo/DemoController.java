d9a25c4fdb86d73afa43edeed014dc3098418a4a
thanhdinh01 <thanhdinhduy0301@gmail.com>
2023-12-14 15:22:55 +0700

    update

--- src/main/java/demojenkins/demo/DemoController.java
+++ src/main/java/demojenkins/demo/DemoController.java
@@ -8,7 +8,7 @@ public class DemoController {
 
     @GetMapping(value = "/about")
     public String about(){
-        return "Hello world1 okea";
+        return "Hello world1 okea123";
     }
     @GetMapping(value = "/contact")
     public String contact(){
